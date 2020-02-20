8f4638db21117313db60298b47a7fde4274afbfe
qinsw <2573992956@qq.com>
2020-02-20 18:37:31 +0800

    init

---
+++ pages/shengmu/shengmu.js
@@ -0,0 +1,148 @@
+// pages/shengmu/shengmu.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [",/。", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"],
+    list2: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+    exam: []
+  },
+
+  boardbtn(e) {
+    var v = e.target.id;
+    var exam = this.data.exam;
+    var target;
+    var i = 0;
+    //获取目标元素
+    for (i; i < exam.length; i++) {
+      if (exam[i].value == false) {
+        target = exam[i].name;
+        break;
+      }
+    }
+
+    if (target == undefined) {
+      wx.showToast({
+        title: '全部正确！请按空格键~',
+        icon: 'none'
+      })
+    } else {
+      //匹配按键字母
+      for (var j = 0; j < v.length; j++) {
+        if (v[j] == target) {
+          exam[i].value = true; //更改判断标识
+          this.setData({
+            exam: exam
+          })
+          break;
+        }
+      }
+    }
+  },
+
+  getRandomArrayElements(arr) {
+    var shuffled = arr.slice(0),
+      i = arr.length,
+      min = i - 5,
+      temp, index;
+    while (i-- > min) {
+      index = Math.floor((i + 1) * Math.random());
+      temp = shuffled[index];
+      shuffled[index] = shuffled[i];
+      shuffled[i] = temp;
+    }
+    return shuffled.slice(min);
+  },
+
+  generate() {
+    var list = this.getRandomArrayElements(this.data.list2);
+    var target = [];
+    for (var i = 0; i < list.length; i++) {
+      var map = {};
+      map.name = list[i];
+      map.value = false;
+      target.push(map);
+    }
+    this.setData({
+      exam: target
+    })
+  },
+
+  blankbtn() {
+    console.log('blank btn clicked...');
+    var exam = this.data.exam;
+    var flag = true;
+    for (var i = 0; i < exam.length; i++) {
+      if (exam[i].value == false) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) {
+      this.generate();
+    } else {
+      wx.showToast({
+        title: '全部输入正确才行哦~',
+        icon: 'none'
+      })
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    this.generate();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
---
+++ pages/yunmu/yunmu.js
@@ -0,0 +1,500 @@
+// pages/yunmu/yunmu.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [],
+    list1: [{
+      name: "a",
+      value: "啊",
+      flag: true
+    }, {
+      name: "o",
+      value: "喔",
+      flag: true
+    }, {
+      name: "e",
+      value: "呃",
+      flag: true
+    }, {
+      name: "i",
+      value: "衣",
+      flag: true
+    }, {
+      name: "u",
+      value: "乌",
+      flag: true
+    }, {
+      name: "ü",
+      value: "迂",
+      flag: true
+    }, {
+      name: "ai",
+      value: "挨",
+      flag: true
+    }, {
+      name: "ei",
+      value: "诶",
+      flag: true
+    }, {
+      name: "ao",
+      value: "熬",
+      flag: true
+    }, {
+      name: "ou",
+      value: "欧",
+      flag: true
+    }, {
+      name: "an",
+      value: "安",
+      flag: true
+    }, {
+      name: "en",
+      value: "恩",
+      flag: true
+    }, {
+      name: "ia",
+      value: "呀",
+      flag: true
+    }, {
+      name: "ie",
+      value: "耶",
+      flag: true
+    }, {
+      name: "iao",
+      value: "腰",
+      flag: true
+    }, {
+      name: "iou",
+      value: "优",
+      flag: true
+    }, {
+      name: "ian",
+      value: "烟",
+      flag: true
+    }, {
+      name: "in",
+      value: "因",
+      flag: true
+    }, {
+      name: "iang",
+      value: "央",
+      flag: true
+    }, {
+      name: "ing",
+      value: "英",
+      flag: true
+    }, {
+      name: "iong",
+      value: "雍",
+      flag: true
+    }, {
+      name: "ua",
+      value: "蛙",
+      flag: true
+    }, {
+      name: "uo",
+      value: "窝",
+      flag: true
+    }, {
+      name: "uai",
+      value: "外",
+      flag: true
+    }, {
+      name: "uen",
+      value: "温",
+      flag: true
+    }, {
+      name: "uang",
+      value: "汪",
+      flag: true
+    }, {
+      name: "ueng",
+      value: "翁",
+      flag: true
+    }, {
+      name: "üe",
+      value: "约",
+      flag: true
+    }, {
+      name: "üan",
+      value: "冤",
+      flag: true
+    }, {
+      name: "ang",
+      value: "昂",
+      flag: true
+    }, {
+      name: "eng",
+      value: "更",
+      flag: true
+    }, {
+      name: "ong",
+      value: "红",
+      flag: true
+    }, {
+      name: "uei",
+      value: "威",
+      flag: true
+    }, {
+      name: "uan",
+      value: "弯",
+      flag: true
+    }],
+    list2: [{
+      name: "b",
+      value: "玻",
+      flag: true
+    }, {
+      name: "p",
+      value: "坡",
+      flag: true
+    }, {
+      name: "m",
+      value: "摸",
+      flag: true
+    }, {
+      name: "f",
+      value: "佛",
+      flag: true
+    }, {
+      name: "d",
+      value: "德",
+      flag: true
+    }, {
+      name: "t",
+      value: "特",
+      flag: true
+    }, {
+      name: "n",
+      value: "讷",
+      flag: true
+    }, {
+      name: "l",
+      value: "勒",
+      flag: true
+    }, {
+      name: "g",
+      value: "哥",
+      flag: true
+    }, {
+      name: "k",
+      value: "科",
+      flag: true
+    }, {
+      name: "h",
+      value: "喝",
+      flag: true
+    }, {
+      name: "j",
+      value: "基",
+      flag: true
+    }, {
+      name: "q",
+      value: "欺",
+      flag: true
+    }, {
+      name: "x",
+      value: "希",
+      flag: true
+    }, {
+      name: "zh",
+      value: "知",
+      flag: true
+    }, {
+      name: "ch",
+      value: "吃",
+      flag: true
+    }, {
+      name: "sh",
+      value: "诗",
+      flag: true
+    }, {
+      name: "r",
+      value: "日",
+      flag: true
+    }, {
+      name: "z",
+      value: "资",
+      flag: true
+    }, {
+      name: "c",
+      value: "雌",
+      flag: true
+    }, {
+      name: "s",
+      value: "思",
+      flag: true
+    }],
+    list3: [{
+      name: "a",
+      value: "A",
+      flag: true
+    }, {
+      name: "b",
+      value: "B",
+      flag: true
+    }, {
+      name: "c",
+      value: "C",
+      flag: true
+    }, {
+      name: "d",
+      value: "D",
+      flag: true
+    }, {
+      name: "e",
+      value: "E",
+      flag: true
+    }, {
+      name: "f",
+      value: "F",
+      flag: true
+    }, {
+      name: "g",
+      value: "G",
+      flag: true
+    }, {
+      name: "h",
+      value: "H",
+      flag: true
+    }, {
+      name: "i",
+      value: "I",
+      flag: true
+    }, {
+      name: "j",
+      value: "J",
+      flag: true
+    }, {
+      name: "k",
+      value: "K",
+      flag: true
+    }, {
+      name: "l",
+      value: "L",
+      flag: true
+    }, {
+      name: "m",
+      value: "M",
+      flag: true
+    }, {
+      name: "n",
+      value: "N",
+      flag: true
+    }, {
+      name: "o",
+      value: "O",
+      flag: true
+    }, {
+      name: "p",
+      value: "P",
+      flag: true
+    }, {
+      name: "q",
+      value: "Q",
+      flag: true
+    }, {
+      name: "r",
+      value: "R",
+      flag: true
+    }, {
+      name: "s",
+      value: "S",
+      flag: true
+    }, {
+      name: "t",
+      value: "T",
+      flag: true
+    }, {
+      name: "u",
+      value: "U",
+      flag: true
+    }, {
+      name: "v",
+      value: "V",
+      flag: true
+    }, {
+      name: "w",
+      value: "W",
+      flag: true
+    }, {
+      name: "x",
+      value: "X",
+      flag: true
+    }, {
+      name: "y",
+      value: "Y",
+      flag: true
+    }, {
+      name: "z",
+      value: "Z",
+      flag: true
+    }],
+    bg1: false,
+    bg2: false,
+    bg3: false,
+    display: false
+  },
+
+  reverse(e) {
+    if (!this.data.display) {
+      var list = this.data.list;
+      var temp = e.currentTarget.dataset.myvalue;
+      var map;
+      for (var i = 0; i < list.length; i++) {
+        map = list[i];
+        if (map.value == temp) {
+          list[i].flag = !map.flag;
+          this.setData({
+            list: list
+          })
+          break;
+        }
+      }
+    }
+  },
+
+  shuffle() {
+    var array = this.data.list;
+    var currentIndex = array.length,
+      temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    this.setData({
+      list: array
+    })
+  },
+
+  resume() {
+    var list = this.data.list;
+    var map;
+    var v = false;;
+    for (var i = 0; i < list.length; i++) {
+      map = list[i];
+      if (!map.flag) {
+        map.flag = true;
+        v = true;
+      }
+    }
+    if (v) {
+      this.setData({
+        list: list
+      })
+    } else {
+      wx.showToast({
+        title: '全都是字母了哦~',
+        icon: 'none'
+      })
+    }
+  },
+
+  choose(e) {
+    var flag = e.target.id;
+    var list;
+    var bg1 = false;
+    var bg2 = false;
+    var bg3 = false;
+    var length = this.data.list.length;
+    if (flag == 1 && length != 34) {
+      list = this.data.list1
+      bg1 = true;
+    } else if (flag == 2 && length != 21) {
+      list = this.data.list2
+      bg2 = true;
+    } else if (flag == 3 && length != 26) {
+      list = this.data.list3
+      bg3 = true;
+    }
+
+    if ((flag == 1 && length == 34) || (flag == 2 && length == 21) || (flag == 3 && length == 26)) {
+
+    } else {
+      this.setData({
+        list: list,
+        bg1: bg1,
+        bg2: bg2,
+        bg3: bg3
+      })
+    }
+
+  },
+
+  displaybtn() {
+    this.setData({
+      display: !this.data.display
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    this.setData({
+      list: this.data.list2,
+      bg2: true
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
