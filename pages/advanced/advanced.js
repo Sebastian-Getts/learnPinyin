40731a44730fee9aad5daabfa50dd58e97b35e13
sebastian <2573992956@qq.com>
2021-04-08 12:05:23 +0800

    limit some attribute that using server

--- pages/advanced/advanced.js
+++ pages/advanced/advanced.js
@@ -154,22 +154,23 @@ Page({
 
   },
   async changeScreen() {
-    try {
-      let {
-        letterOrWord,
-        wordList
-      } = this.data;
-      if (letterOrWord && (wordList.length == 0)) {
-        this.getNewWord();
-      }
-      this.setData({
-        letterOrWord: !this.data.letterOrWord
-      })
-    } catch (error) {
-      await showToast({
-        title: error
-      });
-    }
+    this.test();
+    // try {
+    //   let {
+    //     letterOrWord,
+    //     wordList
+    //   } = this.data;
+    //   if (letterOrWord && (wordList.length == 0)) {
+    //     this.getNewWord();
+    //   }
+    //   this.setData({
+    //     letterOrWord: !this.data.letterOrWord
+    //   })
+    // } catch (error) {
+    //   await showToast({
+    //     title: error
+    //   });
+    // }
   },
 
   async getNewWord() {
